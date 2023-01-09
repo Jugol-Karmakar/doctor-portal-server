c73d7f07cb18106cadccb75517be13c52b6ea4ee
JugolKarmakar <jugol.cse@gmail.com>
2023-01-09 22:25:14 +0600

    vercel commit

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const {
-  MongoClient,
-  ServerApiVersion,
-  ObjectId,
-  ObjectID,
-} = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
