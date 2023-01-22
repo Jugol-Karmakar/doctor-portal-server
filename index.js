99c2bc839b6cf9e7a2b909b9a43bd5d8e41c71da
JugolKarmakar <jugol.cse@gmail.com>
2023-01-22 22:44:36 +0600

    new one added

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iuv9m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iuv9m.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
