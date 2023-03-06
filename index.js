b0f32ccfafdb7066b65cc2971356b3e5f2f5d415
JugolKarmakar <jugol.cse@gmail.com>
2023-03-06 21:58:30 +0600

    deploy commit

--- index.js
+++ index.js
@@ -11,14 +11,11 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iuv9m.mongodb.net/?retryWrites=true&w=majority`;
-
-const MongoClient = require("mongodb").MongoClient;
-
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
   try {
